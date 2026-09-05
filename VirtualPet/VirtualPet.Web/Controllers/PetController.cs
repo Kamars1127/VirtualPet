0b4e53f7bedd774800310c0c02b59b77c2598c70
KMPG\pgkamars <kamars.ct@gmail.com>
2026-09-05 20:02:58 +0800

    no message

---
+++ VirtualPet/VirtualPet.Web/Controllers/PetController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VirtualPet.Application.Services;
+using VirtualPet.Web.Models.Pets;
+
+namespace VirtualPet.Web.Controllers
+{
+    public class PetController : Controller
+    {
+        private readonly PetApplicationService _petApplicationService;
+
+        public PetController(PetApplicationService petApplicationService)
+        {
+            _petApplicationService = petApplicationService ?? throw new ArgumentNullException(nameof(petApplicationService));
+        }
+
+        public async Task<IActionResult> Index(CancellationToken cancellationToken)
+        {
+            var pets = await _petApplicationService.GetPetsAsync(cancellationToken);
+
+            return View(pets);
+        }
+
+        public async Task<IActionResult> Details(Guid id, CancellationToken cancellationToken)
+        {
+            var pet = await _petApplicationService.GetPetAsync(id, cancellationToken);
+
+            if (pet is null) return NotFound();
+
+            var histories = await _petApplicationService.GetPetHistoriesAsync(id, cancellationToken);
+
+            var viewModel = new PetDetailsViewModel
+            {
+                Pet = pet,
+                Histories = histories
+            };
+
+            return View(viewModel);
+        }
+
+    }
+}
---
+++ VirtualPet/VirtualPet.Web/Models/Pets/PetDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using VirtualPet.Application.DTOs;
+
+namespace VirtualPet.Web.Models.Pets
+{
+    public sealed class PetDetailsViewModel
+    {
+        public required PetDto Pet { get; init; }
+
+        public IReadOnlyList<PetHistoryDto> Histories { get; init; } = [];
+    }
+}
